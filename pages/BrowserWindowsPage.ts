e14b4617cd2894dc2dc9315f965f0ce3150ce58c
knackiuz <knackiuz@gmail.com>
2026-03-13 10:12:55 +0200

    added test 'check new window'

--- pages/BrowserWindowsPage.ts
+++ pages/BrowserWindowsPage.ts
@@ -30,4 +30,25 @@ export class BrowserWindowsPage{
         //Close the new tab
         await newPage.close();
     }
+
+    async clickNewWindowAndCheckText(expectedText: string): Promise<void>{
+        //Start waiting for new page(window) before clicking
+        const pagePromise = this.page.context().waitForEvent('page');
+
+        //Perform that action that opens the new window
+        await this.page.locator("#windowButton").click();
+
+        //Wait for the new page object to be created
+        const newPage = await pagePromise;
+
+        //Wait for the new page is loaded
+        await newPage.waitForLoadState();
+
+        //Interact with new page object
+        const headingNewPage = newPage.locator('#sampleHeading');
+        await expect(headingNewPage).toHaveText(expectedText);
+
+        //Close the new window
+        await newPage.close();        
+    }
 }
--- tests/alerts-browser-windows.spec.ts
+++ tests/alerts-browser-windows.spec.ts
@@ -28,4 +28,28 @@ test.describe('Alerts, Frame & Windows Suite @alerts', () => {
         //Click button 'New Tab' and check displayed text
         await browserWindowsPage.clickNewTabAndCheckText(expectedText);
     });
+
+    test('Test for Modal Dialogs: check new window', async ({ page }) => {
+        //Test data
+        const expectedText: string = "This is a sample page";
+
+        //Define main page
+        const mainPage = new MainPage(page);
+
+        //Navigate to main page
+        await mainPage.navigate();
+
+        //Verify that main page ios loaded - check home banner
+        await mainPage.verifyMainPageIsLoaded();
+        
+        //Click on card 'Alerts, Frame & Windows'
+        const alertsFrameWindowsPage = await mainPage.clickOnAlertsFrameWindowsCard();
+
+        //Click on menu 'Browser Windows'
+        const browserWindowsPage = await alertsFrameWindowsPage.selectBrowserWindowsMenu();
+        
+        //Click button 'New Window' and check displayed text
+        await browserWindowsPage.clickNewWindowAndCheckText(expectedText);
+
+    });
 });
